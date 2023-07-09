916d645d093b292a09ecb34fc8fc6d7950a1d936
sayaoyuri <yuriantunes0000@gmail.com>
2023-07-08 21:47:37 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.routes.js';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+dotenv.config();
 
-app.listen(5000, () => console.log('Server is running on http://localhost:5000/'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}/`));
